706c9022f3f1c04104766113024a7ffd0895e616
tilak-blazed <tilak@cloudflare.com>
2025-08-05 12:45:34 +0530

    Commit

--- src/routes/notes.route.ts
+++ src/routes/notes.route.ts
@@ -3,14 +3,32 @@ import { ContextExtended } from "../types";
 
 const notes = new Hono();
 
-notes.get("/", async (ctx: ContextExtended) => {});
+// TODO: In-Workshop Activities.
+notes.get("/", async (ctx: ContextExtended) => {
+  const db = ctx.env.DB;
+  const notes = await db.prepare("SELECT * FROM note LIMIT 50").run();
 
-notes.get("/:id", async (ctx: ContextExtended) => {});
+  return Response.json(notes.results);
+});
 
-notes.post("/", async (ctx: ContextExtended) => {});
+// TODO: In-Workshop Activities.
+notes.get('/:id', async (ctx: ContextExtended) => {
+    return Response.json({})
+});
 
-notes.put("/:id", async (ctx: ContextExtended) => {});
+// TODO: In-Workshop Activities.
+notes.post('/', async (ctx: ContextExtended) => {
+    return Response.json({ message: "note created" })
+});
 
-notes.delete("/:id", async (ctx: ContextExtended) => {});
+// TODO: In-Workshop Activities.
+notes.put('/:id', async (ctx: ContextExtended) => {
+    return Response.json({ message: "note updated" })
+});
+
+// TODO: In-Workshop Activities.
+notes.delete('/:id', async (ctx: ContextExtended) => {
+    return Response.json({ message: "note deleted" })
+});
 
 export default notes;
